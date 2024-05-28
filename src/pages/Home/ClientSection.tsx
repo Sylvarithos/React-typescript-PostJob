b95ecd947d0dd768c9980f501b0aaf3263f6575c
GodricGryffindorK <Godric.Keoni@gmail.com>
2024-05-28 04:54:18 -0800

    dashboard progressing

---
+++ src/pages/Home/ClientSection.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Grid from '@mui/material/Grid';
+function ClientSection() {
+    return (
+        <Grid
+            container
+            direction="row-reverse"
+            justifyContent="center"
+            alignItems="center"
+            // spacing={12}
+            className="bg-custom-grey p-12 m-0 mt-16"
+        >
+            
+            <Grid item className="px-10">
+                <img src="./images/Home/Clients/4.png"></img>
+            </Grid>
+            <Grid item className="px-10">
+                <img src="./images/Home/Clients/3.png"></img>
+            </Grid>
+            <Grid item className="px-10">
+                <img src="./images/Home/Clients/2.png"></img>
+            </Grid>
+            <Grid item className="px-10">
+                <img src="./images/Home/Clients/1.png"></img>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default ClientSection;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,30 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../../features/Home/Header'
 import HeroSection from './HeroSection'
+import ClientSection from './ClientSection'
+import ServiceSection from './ServiceSection/'
+import FindJobSection from './FindJobSection'
+import FeaturedServicesSection from './FeaturedServicesSection'
+import MarketPlaceSection from './MarketPlaceSection'
+import TestimonailsSection from './TestimonailsSection'
+import DownloadSection from './DownloadSection'
+import Footer from './Footer'
 
 function Home() {
     return (
         <div>
             <Header theme='dark' />
             <HeroSection />
+            <div className={`mx-32 mb-12 relative`}>
+                <ClientSection />
+                <ServiceSection />
+                <FindJobSection />
+                <FeaturedServicesSection />
+            </div>
+            <MarketPlaceSection />
+            <TestimonailsSection />
+            <DownloadSection />
+            <Footer />
         </div>
     )
 }
