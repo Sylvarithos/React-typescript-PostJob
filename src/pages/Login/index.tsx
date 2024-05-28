e279e9c2f2614fe2e9afbec4851d43a504960a53
aristotlemartis <aristotlemartis@gmail.com>
2024-05-28 16:11:41 +0300

    login little changed

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -9,76 +9,74 @@ function Login() {
     return (
         <>
             <Header />
-            <div>
-                <Box
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: '40px',
+                }}
+            >
+                <Paper
                     sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '100vh', // Make sure the container spans the entire viewport height
+                        width: '600px',
+                        padding: 4,
+                        textAlign: 'center',
                     }}
+                    elevation={3}
                 >
-                    <Paper
-                        sx={{
-                            width: '600px',
-                            padding: 4,
-                            textAlign: 'center',
-                        }}
-                        elevation={3}
+                    <Typography variant='h4' gutterBottom mb={4}>
+                        Login
+                    </Typography>
+                    <TextField sx={{ marginBottom: 3 }} label='Email' fullWidth />
+                    <TextField sx={{ marginBottom: 3 }} label='Password' type='password' fullWidth />
+                    <div>
+                        <Checkbox defaultChecked />
+                        <Typography>Remember me</Typography>
+                    </div>
+                    <Button variant='text' color='primary'>
+                        Forgot Password
+                    </Button>
+                    <Button size='large' variant='contained' color='secondary' fullWidth sx={{ fontWeight: 'bold', textTransform: 'none' }}>
+                        Login
+                    </Button>
+                    <Divider sx={{ marginY: 3 }}>
+                        <Typography>OR</Typography>
+                    </Divider>
+                    <Button
+                        sx={{ fontWeight: 'bold', textTransform: 'none', marginBottom: 2 }}
+                        size='large'
+                        variant='contained'
+                        color='primary'
+                        fullWidth
+                        startIcon={<FacebookOutlined />}
                     >
-                        <Typography variant='h4' gutterBottom>
-                            Login
-                        </Typography>
-                        <TextField sx={{ marginBotom: 2 }} label='Email' fullWidth />
-                        <TextField sx={{ marginBotom: 2 }} label='Password' type='password' fullWidth />
-                        <div>
-                            <Checkbox defaultChecked />
-                            <Typography>Remember me</Typography>
-                        </div>
-                        <Button variant='text' color='primary'>
-                            Forgot Password
-                        </Button>
-                        <Button size='large' variant='contained' color='secondary' fullWidth sx={{ fontWeight: 'bold', textTransform: 'none' }}>
-                            Login
-                        </Button>
-                        <Divider sx={{ marginY: 3 }}>
-                            <Typography>OR</Typography>
-                        </Divider>
-                        <Button
-                            sx={{ fontWeight: 'bold', textTransform: 'none', marginBottom: 2 }}
-                            size='large'
-                            variant='contained'
-                            color='primary'
-                            fullWidth
-                            startIcon={<FacebookOutlined />}
-                        >
-                            Continue with Facebook
-                        </Button>
-                        <Button
-                            sx={{ fontWeight: 'bold', textTransform: 'none', marginBottom: 2 }}
-                            size='large'
-                            variant='outlined'
-                            color='primary'
-                            fullWidth
-                            startIcon={<Google />}
-                        >
-                            Continue with Google
-                        </Button>
-                        <Typography variant='body2' mb={1}>
-                            Offerin a service?{' '}
-                            <Link to='' style={{ textDecoration: 'underline' }}>
-                                Join as professional
-                            </Link>
-                        </Typography>
-                        <Typography variant='body2'>
-                            Looking for a service?{' '}
-                            <Link to='' style={{ textDecoration: 'underline' }}>
-                                Get Started
-                            </Link>
-                        </Typography>
-                    </Paper>
-                </Box>
-            </div>
+                        Continue with Facebook
+                    </Button>
+                    <Button
+                        sx={{ fontWeight: 'bold', textTransform: 'none', marginBottom: 2 }}
+                        size='large'
+                        variant='outlined'
+                        color='primary'
+                        fullWidth
+                        startIcon={<Google />}
+                    >
+                        Continue with Google
+                    </Button>
+                    <Typography variant='body2' mb={1}>
+                        Offerin a service?{' '}
+                        <Link to='' style={{ textDecoration: 'underline' }}>
+                            Join as professional
+                        </Link>
+                    </Typography>
+                    <Typography variant='body2'>
+                        Looking for a service?{' '}
+                        <Link to='' style={{ textDecoration: 'underline' }}>
+                            Get Started
+                        </Link>
+                    </Typography>
+                </Paper>
+            </Box>
         </>
     )
 }
